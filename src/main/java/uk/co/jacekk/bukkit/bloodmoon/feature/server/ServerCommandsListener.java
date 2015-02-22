58cc23d1f9ee9c4e48a9e0d30e835ad9fff21d31
Samistine <samuel@samistine.com>
2015-02-21 22:04:47 -0500

    [New Feature] Add delay to commands being issued
    Also starting to use correct version info, going from 0.25 to 0.30

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/server/ServerCommandsListener.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/server/ServerCommandsListener.java
@@ -1,8 +1,12 @@
 package uk.co.jacekk.bukkit.bloodmoon.feature.server;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import uk.co.jacekk.bukkit.baseplugin.config.PluginConfig;
 import uk.co.jacekk.bukkit.baseplugin.event.BaseListener;
@@ -26,6 +30,42 @@ public class ServerCommandsListener extends BaseListener<BloodMoon> {
 		
 		if (plugin.isFeatureEnabled(worldName, Feature.SERVER_COMMANDS)){
 			for (String command : worldConfig.getStringList(Config.FEATURE_SERVER_COMMANDS_START_COMMANDS)){
+				if (command.startsWith("(")) {
+					String mydata = "()some with 'the data i want' inside";
+					Pattern pattern = Pattern.compile("^(\\(([^()]*)\\))");
+					Matcher matcher = pattern.matcher(mydata);
+					if (matcher.find())
+					{ //found ()||(x) at beginning of line
+						String group2 = matcher.group(2); //group 2 should be just x without ()
+						if (!group2.isEmpty()) { //check if it is empty
+							int time;
+							try {
+								time = Integer.parseInt(group2);
+							} catch(NumberFormatException e) {
+								plugin.getLogger().severe("ERROR PARSING INTEGER, in config, please fix line: " + command);
+								return;
+							}
+							String replaceRegex = "//(" + time + "//)";
+							String commandToRun = command.replaceFirst(replaceRegex, "");
+							if (commandToRun.startsWith(" ")) {
+								commandToRun = commandToRun.replaceFirst(" ", "");
+							}
+                                                        
+                                                        final String finalCommand = commandToRun;
+                                                        
+							BukkitScheduler scheduler = plugin.getServer().getScheduler();
+							scheduler.scheduleSyncDelayedTask(plugin, new Runnable() {
+                                                            
+								@Override
+								public void run() {
+									plugin.server.dispatchCommand(plugin.server.getConsoleSender(), finalCommand);
+								}
+							}, time);
+                                                        return;
+						}
+					}
+
+				}
 				plugin.server.dispatchCommand(plugin.server.getConsoleSender(), command);
 			}
 		}
