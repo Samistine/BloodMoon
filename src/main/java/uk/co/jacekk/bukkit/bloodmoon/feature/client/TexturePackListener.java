ea1b68dd11fdaa66c6b07a31e8cf2863cafe0e1b
Samistine <samuel@samistine.com>
2015-03-30 14:07:21 -0400

    Better threading speed

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/client/TexturePackListener.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/client/TexturePackListener.java
@@ -1,5 +1,6 @@
 package uk.co.jacekk.bukkit.bloodmoon.feature.client;
 
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,60 +16,87 @@ import uk.co.jacekk.bukkit.bloodmoon.event.BloodMoonEndEvent;
 import uk.co.jacekk.bukkit.bloodmoon.event.BloodMoonStartEvent;
 
 public class TexturePackListener extends BaseListener<BloodMoon> {
-	
-	public TexturePackListener(BloodMoon plugin){
-		super(plugin);
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-	public void onBloodMoonStart(BloodMoonStartEvent event){
-		String worldName = event.getWorld().getName();
-		PluginConfig worldConfig = plugin.getConfig(worldName);
-		
-		if (plugin.isFeatureEnabled(worldName, Feature.TEXTURE_PACK)){
-			for (Player player : event.getWorld().getPlayers()){
-				player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_BLOODMOON));
-			}
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-	public void onBloodMoonEnd(BloodMoonEndEvent event){
-		String worldName = event.getWorld().getName();
-		PluginConfig worldConfig = plugin.getConfig(worldName);
-		
-		if (plugin.isFeatureEnabled(worldName, Feature.TEXTURE_PACK)){
-			for (Player player : event.getWorld().getPlayers()){
-				player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_NORMAL));
-			}
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onPlayerChangedWorld(PlayerChangedWorldEvent event){
-		Player player = event.getPlayer();
-		String fromName = event.getFrom().getName();
-		String toName = player.getWorld().getName();
-		PluginConfig worldConfig = plugin.getConfig(toName);
-		
-		if (plugin.isFeatureEnabled(toName, Feature.TEXTURE_PACK)){
-			if (!plugin.isActive(fromName) && plugin.isActive(toName)){
-				player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_BLOODMOON));
-			}else if (plugin.isActive(fromName) && !plugin.isActive(toName)){
-				player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_NORMAL));
-			}
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-	public void onPlayerJoin(PlayerJoinEvent event){
-		Player player = event.getPlayer();
-		String worldName = player.getWorld().getName();
-		PluginConfig worldConfig = plugin.getConfig(worldName);
-		
-		if (plugin.isActive(worldName) && plugin.isFeatureEnabled(worldName, Feature.TEXTURE_PACK)){
-			player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_BLOODMOON));
-		}
-	}
-	
+
+    public TexturePackListener(BloodMoon plugin) {
+        super(plugin);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBloodMoonStart(BloodMoonStartEvent event) {
+        final World world = event.getWorld();
+
+        if (plugin.isFeatureEnabled(world.getName(), Feature.TEXTURE_PACK)) {
+            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                
+                @Override
+                public void run() {
+                    sendResourcePack(world, true);
+                }
+            });
+            
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBloodMoonEnd(BloodMoonEndEvent event) {
+        final World world = event.getWorld();
+
+        if (plugin.isFeatureEnabled(world.getName(), Feature.TEXTURE_PACK)) {
+            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+
+                @Override
+                public void run() {
+                    sendResourcePack(world, false);
+                }
+            });
+            
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        String fromName = event.getFrom().getName();
+        String toName = player.getWorld().getName();
+        PluginConfig worldConfig = plugin.getConfig(toName);
+
+        if (plugin.isFeatureEnabled(toName, Feature.TEXTURE_PACK)) {
+            if (!plugin.isActive(fromName) && plugin.isActive(toName)) {
+                player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_BLOODMOON));
+            } else if (plugin.isActive(fromName) && !plugin.isActive(toName)) {
+                player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_NORMAL));
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String worldName = player.getWorld().getName();
+        PluginConfig worldConfig = plugin.getConfig(worldName);
+
+        if (plugin.isActive(worldName) && plugin.isFeatureEnabled(worldName, Feature.TEXTURE_PACK)) {
+            player.setResourcePack(worldConfig.getString(Config.FEATURE_TEXTURE_PACK_BLOODMOON));
+        }
+    }
+
+    /**
+     * Send the resource pack to all players in the specified world True
+     *
+     * @param world
+     * @param special true:TEXTURE_PACK_BLOODMOON false:TEXTURE_PACK_NORMAL
+     */
+    public void sendResourcePack(World world, boolean special) {
+        PluginConfig worldConfig = plugin.getConfig(world.getName());
+        String resource;
+        if (special) {
+            resource = worldConfig.getString(Config.FEATURE_TEXTURE_PACK_BLOODMOON);
+        } else {
+            resource = worldConfig.getString(Config.FEATURE_TEXTURE_PACK_NORMAL);
+        }
+
+        for (Player player : world.getPlayers()) {
+            player.setResourcePack(resource);
+        }
+    }
 }
