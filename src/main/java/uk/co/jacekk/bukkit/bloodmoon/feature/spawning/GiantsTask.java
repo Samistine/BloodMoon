e25da44bd2a59c7cf1bbdf0c87272511e0dcd69d
Samistine <samuel@samistine.com>
2015-06-10 19:01:52 -0400

    Reformat Code

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/spawning/GiantsTask.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/spawning/GiantsTask.java
@@ -13,47 +13,48 @@ import uk.co.jacekk.bukkit.bloodmoon.nms.EntityGiantZombie;
 import java.util.Random;
 
 public class GiantsTask extends BaseTask<BloodMoon> {
-	
-	private final CraftWorld world;
-	private final Random random = new Random();
-	
-	public GiantsTask(BloodMoon plugin, CraftWorld world){
-		super(plugin);
-		
-		this.world = world;
-	}
-	
-	@Override
-	public void run(){
-		long worldTime = this.world.getTime();
-		
-		if (worldTime < 13000 || worldTime > 23000){
-			return;
-		}
-		
-		spawn: for (Chunk chunk : this.world.getLoadedChunks()){
-			if (this.random.nextInt(100) == 1){
-				int x = (chunk.getX() * 16) + this.random.nextInt(12) + 2;
-				int z = (chunk.getZ() * 16) + this.random.nextInt(12) + 2;
-				int y = this.world.getHighestBlockYAt(x, z);
-				
-				Location spawnLocation = new Location(world, x, y, z);
-				
-				for (Entity entity : this.world.getLivingEntities()){
-					if (!entity.isDead() && entity.getLocation().distanceSquared(spawnLocation) < 1024){
-						continue spawn;
-					}
-				}
-				
-				World nmsWorld = this.world.getHandle();
-				
-				EntityGiantZombie entity = new EntityGiantZombie(nmsWorld);
-				
-				entity.setPositionRotation(x, y, z, 0, 90);
-				nmsWorld.addEntity(entity, SpawnReason.CUSTOM);
-				entity.p(null);
-			}
-		}
-	}
-	
+
+    private final CraftWorld world;
+    private final Random random = new Random();
+
+    public GiantsTask(BloodMoon plugin, CraftWorld world) {
+        super(plugin);
+
+        this.world = world;
+    }
+
+    @Override
+    public void run() {
+        long worldTime = this.world.getTime();
+
+        if (worldTime < 13000 || worldTime > 23000) {
+            return;
+        }
+
+        spawn:
+        for (Chunk chunk : this.world.getLoadedChunks()) {
+            if (this.random.nextInt(100) == 1) {
+                int x = (chunk.getX() * 16) + this.random.nextInt(12) + 2;
+                int z = (chunk.getZ() * 16) + this.random.nextInt(12) + 2;
+                int y = this.world.getHighestBlockYAt(x, z);
+
+                Location spawnLocation = new Location(world, x, y, z);
+
+                for (Entity entity : this.world.getLivingEntities()) {
+                    if (!entity.isDead() && entity.getLocation().distanceSquared(spawnLocation) < 1024) {
+                        continue spawn;
+                    }
+                }
+
+                World nmsWorld = this.world.getHandle();
+
+                EntityGiantZombie entity = new EntityGiantZombie(nmsWorld);
+
+                entity.setPositionRotation(x, y, z, 0, 90);
+                nmsWorld.addEntity(entity, SpawnReason.CUSTOM);
+                entity.p(null);
+            }
+        }
+    }
+
 }
